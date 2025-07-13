74df77beeecc4711645d401e57ce493bc50afb49
Cintoma123 <calistusumehnnachebe@gmail.com>
2025-07-13 17:10:09 +0100

    updated my schema

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,7 +1,16 @@
 import graphene
 from graphene import ObjectType, Schema
 from graphene_django.types import DjangoObjectType
+import graphene
+from crm.schema import Query as CRMQuery, Mutation as CRMMutation
 
-class Query(ObjectType):
+class Query(CRMQuery, graphene.ObjectType):
+    pass
+
+class Mutation(CRMMutation, graphene.ObjectType):
+    pass
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
+"""class Query(ObjectType):
     class Meta:
-        field = (name:'hello', Types:'world')
+        field = (name:'hello', Types:'world')"""
--- graph_crm/schema.py
+++ graph_crm/schema.py
@@ -3,7 +3,17 @@ from graphene import ObjectType, List
 from graphene_django.types import DjangoObjectType
 from django.db import transaction
 from .models import Customer, Product, Order
-class Query(ObjectType):
+import graphene
+from crm.schema import Query as CRMQuery, Mutation as CRMMutation
+
+class Query(CRMQuery, graphene.ObjectType):
+    pass
+
+class Mutation(CRMMutation, graphene.ObjectType):
+    pass
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
+"""class Query(ObjectType):
     customers = List(CustomerType)
     products = List(ProductType)
     orders = List(OrderType)
@@ -15,4 +25,4 @@ class Query(ObjectType):
         return Product.objects.all()
 
     def resolve_orders(self, info):
-        return Order.objects.all()
+        return Order.objects.all()"""
